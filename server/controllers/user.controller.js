e40019e5961577535ab3ccad00f25a48664669fb
vinodpatil2002 <vinodnspatil@gmail.com>
2024-03-20 01:38:59 +0530

    Add logout functionality to user controller and route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -97,3 +97,12 @@ export const login = async (req, res) => {
         return res.status(500).json({ msg: error.message });
     }
 };
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('refreshtoken', { path: '/user/refresh_token' });
+        return res.json({ msg: "Logged out" });
+    } catch (err) {
+        return res.status(500).json({ msg: err.message });
+    }
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { refreshToken, registerUser,login } from "../controllers/user.controller.js";
+import { refreshToken, registerUser,login,logout } from "../controllers/user.controller.js";
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/refresh_token', refreshToken);
 router.post('/login', login);
+router.post('/logout', logout);
 
 export default router;
